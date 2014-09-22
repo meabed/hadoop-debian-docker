ffe7885ec409d60e6e5b3c7ebc82d576672d6aea
Mohamed Meabed <mo.meabed@gmail.com>
2014-09-22 03:20:33 +0200

    INIT

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,9 @@ service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
 
+# Save Enviroemnt Variables incase ot attach to the container with new tty
+env |  awk '{split($0,a,"\n"); print "export " a[1]}' > /etc/env_profile
+
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
 fi
